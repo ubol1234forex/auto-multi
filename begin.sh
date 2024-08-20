b77c29d4b5f08c8d79274b5dc417ba102ec27434
ubol1234forex <ubol1234forex@gmail.com>
2024-08-20 21:55:44 +0700

    Update begin.sh

--- begin.sh
+++ begin.sh
@@ -7,7 +7,7 @@ yes | pkg install libjansson wget nano
 cp /data/data/com.termux/files/usr/include/linux/sysctl.h /data/data/com.termux/files/usr/include/sys
 
 # Clone repository 
-git clone https://github.com/ubol1234forex/jk8180.git
+git clone https://github.com/ubol1234forex/jk8180-multi.git
 cd jk8180-multi
 git clone https://github.com/ubol1234forex/set-miner-off.git
 
