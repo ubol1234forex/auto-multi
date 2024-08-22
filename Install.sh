8183436e273830023838dfad55e2a69a77169e59
ubol1234forex <ubol1234forex@gmail.com>
2024-08-22 23:07:16 +0700

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -1,2 +1,2 @@
-curl -o- -k https://raw.githubusercontent.com/ubol1234forex/auto-multi/main/begin.sh | bash
-curl -o- -k https://raw.githubusercontent.com/ubol1234forex/auto-multi/main/begin2.sh | bash
+https://raw.githubusercontent.com/ubol1234forex/auto-multi/main/begin.sh
+https://raw.githubusercontent.com/ubol1234forex/auto-multi/main/begin2.sh
