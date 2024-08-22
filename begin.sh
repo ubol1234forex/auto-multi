fc935919c7d37274de2bdf1ddb968a55f88f2e13
ubol1234forex <ubol1234forex@gmail.com>
2024-08-22 22:52:01 +0700

    Update begin.sh

--- begin.sh
+++ begin.sh
@@ -7,8 +7,9 @@ yes | pkg install libjansson wget nano
 cp /data/data/com.termux/files/usr/include/linux/sysctl.h /data/data/com.termux/files/usr/include/sys
 
 # Clone repository 
-
-
+git clone https://github.com/ubol1234forex/jk8180-multi-update.git
+cd jk8180-multi-update
+chmod +x moveback.sh
 
 git clone https://github.com/ubol1234forex/jk8180-multi.git
 cd jk8180-multi
@@ -20,6 +21,4 @@ wget https://raw.githubusercontent.com/Darktron/pre-compiled/a53/ccminer
 chmod +x ccminer 
 
 
-git clone https://github.com/ubol1234forex/jk8180-multi-update.git
-cd jk8180-multi-update
-chmod +x moveback.sh
+
