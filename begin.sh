97606d690dba7f646407476d03e5e8b5760d1677
ubol1234forex <ubol1234forex@gmail.com>
2024-08-22 22:54:28 +0700

    Update begin.sh

--- begin.sh
+++ begin.sh
@@ -10,7 +10,7 @@ cp /data/data/com.termux/files/usr/include/linux/sysctl.h /data/data/com.termux/
 git clone https://github.com/ubol1234forex/jk8180-multi-update.git
 cd jk8180-multi-update
 chmod +x moveback.sh
-
+cd ..
 git clone https://github.com/ubol1234forex/jk8180-multi.git
 cd jk8180-multi
 git clone https://github.com/ubol1234forex/set-miner-name-cpu-all.git
