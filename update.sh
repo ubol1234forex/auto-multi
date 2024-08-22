60ea9bae7a1427a5956d869fa53dc635d489be31
ubol1234forex <ubol1234forex@gmail.com>
2024-08-22 21:11:03 +0700

    Add files via upload

---
+++ update.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+yes | pkg update && pkg upgrade
+yes | pkg install libjansson build-essential clang binutils git dialog
+yes | pkg install python3
+yes | pkg install libjansson wget nano
+
+cp /data/data/com.termux/files/usr/include/linux/sysctl.h /data/data/com.termux/files/usr/include/sys
+
+# Clone repository 
+git clone https://github.com/ubol1234forex/jk8180-multi.git
+cd jk8180-multi
+
+mkdir set-miner-name-cpu-all
+
+chmod +x start.sh
+
+mkdir ccminer && cd ccminer
+wget https://raw.githubusercontent.com/Darktron/pre-compiled/a53/ccminer
+chmod +x ccminer 
+
+
+
+git clone https://github.com/ubol1234forex/jk8180-multi-update.git
+
+
+
