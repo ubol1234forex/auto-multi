90bf46936fd3f732447217939c65bae9628d7054
ubol1234forex <ubol1234forex@gmail.com>
2024-08-23 14:15:37 +0700

    Update begin.sh

--- begin.sh
+++ begin.sh
@@ -21,8 +21,7 @@ mkdir ccminer && cd ccminer
 wget https://raw.githubusercontent.com/Darktron/pre-compiled/a53/ccminer
 chmod +x ccminer 
 
-cd~
-cd jk8180-multi && mkdir hansen33s-dero-miner-android-arm64 && cd hansen33s-dero-miner-android-arm64
+cd~&&cd jk8180-multi && mkdir hansen33s-dero-miner-android-arm64 && cd hansen33s-dero-miner-android-arm64
 wget https://github.com/Hansen333/Hansen33-s-DERO-Miner/releases/download/Version-0.6/hansen33s-dero-miner-android-arm64.tar.gz
 tar -xf hansen33s-dero-miner-android-arm64.tar.gz
 
