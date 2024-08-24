cdcc136d272173212bf896e369256caf717c3b3e
ubol1234forex <ubol1234forex@gmail.com>
2024-08-24 12:24:14 +0700

    Update begin.sh

--- begin.sh
+++ begin.sh
@@ -25,11 +25,15 @@ cd jk8180-multi
 git clone https://github.com/ubol1234forex/set-miner-name-cpu-all.git
 chmod +x start.sh move.sh 
 
+#ccminer
 mkdir miner && cd miner
 wget https://raw.githubusercontent.com/Darktron/pre-compiled/a53/ccminer
 chmod +x ccminer 
 
+#hansen333
 wget https://github.com/Hansen333/Hansen33-s-DERO-Miner/releases/download/Version-0.6/hansen33s-dero-miner-android-arm64.tar.gz
-
 tar -xf hansen33s-dero-miner-android-arm64.tar.gz
 
+#xmrigcc
+wget https://github.com/Bendr0id/xmrigCC/releases/download/3.4.2/xmrigCC-miner_only-3.4.2-android-dynamic-arm64.zip
+unzip xmrigCC-miner_only-3.4.2-android-dynamic-arm64.zip
