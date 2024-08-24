32ea5e5dc69ad47afe6a1b5f27404e79574b91c5
ubol1234forex <ubol1234forex@gmail.com>
2024-08-24 14:39:23 +0700

    Update update.sh

--- update.sh
+++ update.sh
@@ -14,13 +14,15 @@ mkdir set-miner-name-cpu-all
 chmod +x start.sh move.sh
 
 
+#ccminer
 mkdir miner && cd miner
 wget https://raw.githubusercontent.com/Darktron/pre-compiled/a53/ccminer
 chmod +x ccminer 
 
+#hansen333
 wget https://github.com/Hansen333/Hansen33-s-DERO-Miner/releases/download/Version-0.6/hansen33s-dero-miner-android-arm64.tar.gz
-
 tar -xf hansen33s-dero-miner-android-arm64.tar.gz
 
-
-
+#xmrigcc
+wget https://github.com/Bendr0id/xmrigCC/releases/download/3.4.2/xmrigCC-miner_only-3.4.2-android-dynamic-arm64.zip
+unzip xmrigCC-miner_only-3.4.2-android-dynamic-arm64.zip
